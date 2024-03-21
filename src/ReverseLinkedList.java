1a0fd646c32cbf420a9512deab195c5650e2315d
AakashAkshat <aakashakshat2002@gmail.com>
2024-03-21 22:24:12 +0530

    206. Reverse LinkedList

---
+++ src/ReverseLinkedList.java
@@ -0,0 +1,18 @@
+public class ReverseLinkedList {
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            if(head == null)
+                return head;
+
+            if(head.next == null)
+                return head;
+
+            ListNode newHead = reverseList(head.next);
+
+            head.next.next = head;
+            head.next = null;
+
+            return newHead;
+        }
+    }
+}
