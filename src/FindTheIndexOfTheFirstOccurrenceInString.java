f1c35ac4e013d08f3c3ae8f351076cfce8468195
AakashAkshat <aakashakshat2002@gmail.com>
2024-04-02 00:40:00 +0530

    28. Find the Index of the First Occurrence in a String

---
+++ src/FindTheIndexOfTheFirstOccurrenceInString.java
@@ -0,0 +1,15 @@
+public class FindTheIndexOfTheFirstOccurrenceInString {
+
+    class Solution {
+        public int strStr(String haystack, String needle) {
+            int length = needle.length();
+
+            for(int i=0; i+length <= haystack.length(); i++) {
+                if(haystack.substring(i, i+length).equals(needle))
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
