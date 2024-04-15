e3fddeada6f62cd2dba6aefd1b39140b70bcc6f9
AakashAkshat <aakashakshat2002@gmail.com>
2024-04-15 22:05:42 +0530

    129. Sum Root to Leaf Numbers

---
+++ src/SumRootToLeafNumbers.java
@@ -0,0 +1,26 @@
+public class SumRootToLeafNumbers {
+    class Solution {
+        private int ans = 0;
+
+        public int sumNumbers(TreeNode root)
+        {
+            dfs(root, 0);
+            return ans;
+        }
+
+        private void dfs(TreeNode root, int path)
+        {
+            if (root==null)
+                return;
+
+            if (root.left == null && root.right == null)
+            {
+                ans += path * 10 + root.val;
+                return;
+            }
+
+            dfs(root.left, path * 10 + root.val);
+            dfs(root.right, path * 10 + root.val);
+        }
+    }
+}
