cc3d319a0e4d0c9d6ed5d3a8df7d2fb57a1ade38
AakashAkshat <aakashakshat2002@gmail.com>
2024-06-01 23:20:10 +0530

    3110. Score of a string

---
+++ src/ScoreOfAString.java
@@ -0,0 +1,12 @@
+public class ScoreOfAString {
+    class Solution {
+        public int scoreOfString(String s) {
+            int ans = 0;
+
+            for (int i = 1; i < s.length(); ++i)
+                ans += Math.abs(s.charAt(i) - s.charAt(i - 1));
+
+            return ans;
+        }
+    }
+}
