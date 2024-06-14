5de129a3d0244be21882c44377ce59f416701272
AakashAkshat <aakashakshat2002@gmail.com>
2024-06-14 22:17:59 +0530

    945. Minimum Increment To make Array Unique

---
+++ src/MinimumIncrementToMakeArrayUnique.java
@@ -0,0 +1,20 @@
+public class MinimumIncrementToMakeArrayUnique {
+    class Solution
+    {
+        public int minIncrementForUnique(int[] nums)
+        {
+            int ans = 0;
+            int min = 0;
+
+            Arrays.sort(nums);
+
+            for (int num : nums)
+            {
+                ans += Math.max(min - num, 0);
+                min = Math.max(min, num) + 1;
+            }
+
+            return ans;
+        }
+    }
+}
